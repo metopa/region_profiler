0e80aa4ef2af685922d1506b64338fe7c2a92008
Viacheslav Kroilov <slavakroilov@gmail.com>
2019-01-03 17:36:27 +0100

    Test reporters

--- tests/test_reporters.py
+++ tests/test_reporters.py
@@ -1,5 +1,8 @@
+import pytest
+
 from region_profiler import RegionProfiler
 from region_profiler.reporters import *
+from region_profiler import reporter_columns as cols
 
 
 class FixedStats:
@@ -10,7 +13,10 @@ class FixedStats:
         self.max = max
 
 
-def test_slice_generation():
+@pytest.fixture()
+def dummy_region_profiler():
+    """Generate dummy region profiler to test reporters.
+    """
     rp = RegionProfiler()
 
     with rp.region('a'):
@@ -31,6 +37,12 @@ def test_slice_generation():
     a.children['d'].stats = FixedStats(1, 25, 25, 25)
     a.children['d'].children['x'].stats = FixedStats(1, 10, 10, 10)
 
+    return rp
+
+
+def test_slice_generation(dummy_region_profiler):
+    """Test that node tree is properly serialized in a list.
+    """
     expected = [Slice(0, '<root>', None, 0, 1, 100, 10, 100, 100),
                 Slice(1, 'a', None, 1, 1, 90, 15, 90, 90),
                 Slice(2, 'c', None, 2, 2, 30, 20, 10, 20),
@@ -46,7 +58,79 @@ def test_slice_generation():
     expected[5].parent = expected[4]
     expected[6].parent = expected[1]
 
-    slices = get_profiler_slice(rp)
+    slices = get_profiler_slice(dummy_region_profiler)
     assert slices == expected
 
 
+def test_silent_reporter(dummy_region_profiler):
+    """Test :py:class:`SilentReporter` reporter.
+    """
+    r = SilentReporter([cols.name, cols.node_id, cols.parent_id, cols.total_us])
+
+    r.dump_profiler(dummy_region_profiler)
+
+    expected = [['name', 'id', 'parent_id', 'total_us'],
+                ['<root>', '0', '', '100000000'],
+                ['a', '1', '0', '90000000'],
+                ['c', '2', '1', '30000000'],
+                ['x', '3', '2', '10000000'],
+                ['d', '4', '1', '25000000'],
+                ['x', '5', '4', '10000000'],
+                ['b', '6', '1', '20000000']]
+
+    assert r.rows == expected
+
+
+def test_console_reporter(dummy_region_profiler, capsys):
+    """Test :py:class:`ConsoleReporter` reporter.
+    """
+    r = ConsoleReporter([cols.name, cols.node_id, cols.parent_id, cols.total_us],
+                        stream=sys.stdout)
+
+    r.dump_profiler(dummy_region_profiler)
+
+    expected = [['name', 'id', 'parent id', 'total us'],
+                [],
+                ['<root>', '0', '', '100000000'],
+                ['a', '1', '0', '90000000'],
+                ['c', '2', '1', '30000000'],
+                ['x', '3', '2', '10000000'],
+                ['d', '4', '1', '25000000'],
+                ['x', '5', '4', '10000000'],
+                ['b', '6', '1', '20000000']]
+
+    output, err = capsys.readouterr()
+    output = output.strip().split('\n')
+    assert len(output) == len(expected)
+
+    for row, expected_vals in zip(output, expected):
+        assert len(row) == len(output[0])
+        for v in expected_vals:
+            assert v in row
+
+
+def test_csv_reporter(dummy_region_profiler, capsys):
+    """Test :py:class:`CsvReporter` reporter.
+    """
+    r = CsvReporter([cols.name, cols.node_id, cols.parent_id, cols.total_us],
+                    stream=sys.stdout)
+
+    r.dump_profiler(dummy_region_profiler)
+
+    expected = [['name', 'id', 'parent_id', 'total_us'],
+                ['<root>', '0', '', '100000000'],
+                ['a', '1', '0', '90000000'],
+                ['c', '2', '1', '30000000'],
+                ['x', '3', '2', '10000000'],
+                ['d', '4', '1', '25000000'],
+                ['x', '5', '4', '10000000'],
+                ['b', '6', '1', '20000000']]
+
+    output, err = capsys.readouterr()
+    output = [[c.strip() for c in r.split(',')] for r in output.strip().split('\n')]
+
+    assert len(output) == len(expected)
+    for row, expected_vals in zip(output, expected):
+        assert len(row) == len(expected_vals)
+        for col, v in zip(row, expected_vals):
+            assert col == v
